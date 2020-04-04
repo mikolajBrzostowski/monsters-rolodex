18f2e39d23cc904863929e37b7fb8484b063e5aa
mikolaj.brzostowski@gmail.com <mikolaj.brzostowski@gmail.com>
2020-04-04 15:57:20 +0200

    Updates

---
+++ precache-manifest.341319614a9219c301af645340a22916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aed9037317fe7c5caa63afaa8c1088e",
+    "url": "/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "bf0d9ff02ab2891d0f5a",
+    "url": "/monsters-rolodex.git/static/css/main.e9dc6736.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex.git/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex.git/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf0d9ff02ab2891d0f5a",
+    "url": "/monsters-rolodex.git/static/js/main.476022a1.chunk.js"
+  },
+  {
+    "revision": "d8764b1a334515788546",
+    "url": "/monsters-rolodex.git/static/js/runtime-main.aa714bd1.js"
+  }
+]);
